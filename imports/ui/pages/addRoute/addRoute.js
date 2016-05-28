ad2f77faeca545d483c7bc42a3617e19ec5fc172
Malith <malit.tilak@gmail.com>
2016-05-28 14:39:19 +0530

    Complete Add route--> drawing the route path on map function

--- imports/ui/pages/addRoute/addRoute.js
+++ imports/ui/pages/addRoute/addRoute.js
@@ -1,6 +1,14 @@
 import './addRoute.html';
 
 var ZOOM_LEVEL = 10; // ideal zoom level for streets
+var startingPoint;
+var endingPoint;
+var waypointsArray = [];
+var startSelectingState = false;
+var endSelectingState = false;
+var addMarkerState;
+var directionsDisplay;
+var directionsService;
 
 Template.addRouteMap.helpers({
     geolocationError: function () {
@@ -46,15 +54,122 @@ Template.addRouteMap.helpers({
 
 });
 
+Template.addRouteMap.events({
+    'click #startingPoint'(event) {
+        startSelectingState = true;
+        disableEnable(true);
+    },
+    'click #endingPoint'(event) {
+        endSelectingState = true;
+        disableEnable(true);
 
-Template.addRouteMap.onCreated(function() {
+
+    },
+    'click #addMarker'(event) {
+        addMarkerState = true;
+        document.getElementById('addMarker').disabled = true;
+    },
+    'click #roughRoute'(event) {
+
+        GoogleMaps.ready('addRouteMap', function (addRouteMap) {
+            // clear previous values if..
+
+            directionsService = new google.maps.DirectionsService;
+            directionsDisplay = new google.maps.DirectionsRenderer({
+                draggable: true,
+                map: addRouteMap.instance,
+                panel: document.getElementById('right-panel')
+            });
 
 
-    GoogleMaps.ready('addRouteMap', function(addRouteMap) {
+            directionsDisplay.addListener('directions_changed', function () {
+                computeTotalDistance(directionsDisplay.getDirections());
+            });
+
+            displayRoute(startingPoint, endingPoint, directionsService,
+                directionsDisplay);
+        })
+
+        document.getElementById('addRouteButtons1').style.height = 0;
+        document.getElementById('addRouteButtons1').style.visibility = 'hidden';
+
+    }
+
+});
+
+Template.addRouteMap.onCreated(function () {
+
+
+    GoogleMaps.ready('addRouteMap', function (addRouteMap) {
         console.log("I'm ready!");
 
+        // set default points
+        startingPoint = new google.maps.LatLng(6.522755, 80.114548);
+        endingPoint = new google.maps.LatLng(6.593758, 79.958979);
 
+        google.maps.event.addListener(addRouteMap.instance, 'click', function (event) {
+            if (startSelectingState) {
+                startingPoint = new google.maps.LatLng(event.latLng.lat(), event.latLng.lng());
+                startSelectingState = false;
+                disableEnable(false);
+            }
+            else if (endSelectingState) {
+                endingPoint = new google.maps.LatLng(event.latLng.lat(), event.latLng.lng());
+                endSelectingState = false;
+                disableEnable(false);
+            }
+            else if (addMarkerState) {
+                waypointsArray.push({location: new google.maps.LatLng(event.latLng.lat(), event.latLng.lng())});
+                console.log(waypointsArray);
+                displayRoute(startingPoint, endingPoint, directionsService,
+                    directionsDisplay);
+                addMarkerState = false;
+                document.getElementById('addMarker').disabled = false;
+            }
+
+
+
+        });
 
 
     });
 });
+
+function displayRoute(origin, destination, service, display) {
+    service.route({
+        origin: origin,
+        destination: destination,
+        waypoints: waypointsArray,
+        travelMode: google.maps.TravelMode.DRIVING,
+        avoidTolls: true,
+        avoidHighways: true,
+        region: 'LK',
+        provideRouteAlternatives: false,
+        unitSystem: google.maps.UnitSystem.METRIC
+
+    }, function (response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+            display.setDirections(response);
+        } else {
+            alert('Could not display directions due to: ' + status);
+        }
+    });
+}
+
+function computeTotalDistance(result) {
+    var total = 0;
+    var myroute = result.routes[0];
+    for (var i = 0; i < myroute.legs.length; i++) {
+        total += myroute.legs[i].distance.value;
+    }
+    total = total / 1000;
+    document.getElementById('total').innerHTML = total + ' km';
+}
+
+function disableEnable(value) {
+    document.getElementById('startingPoint').disabled = value;
+    document.getElementById('endingPoint').disabled = value;
+    document.getElementById('addMarker').disabled = value;
+    document.getElementById('roughRoute').disabled = value;
+
+}
