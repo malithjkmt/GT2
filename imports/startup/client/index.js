93a56917f27db5c4fc3b178fdbb9f64b0a95927a
Malith <malit.tilak@gmail.com>
2016-06-03 10:32:22 +0530

    Right before begin testing

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -2,6 +2,6 @@ import './route.js';
 import './autoform.js';
 
 Meteor.startup(function () {
-    GoogleMaps.load({ key: 'AIzaSyAHM8gxbuwZEThMNM0DUYbP-vgvIxUQELs'});
+    GoogleMaps.load({ key: 'AIzaSyAHM8gxbuwZEThMNM0DUYbP-vgvIxUQELs',libraries:'geometry,places,drawing'});
     //GoogleMaps.load({ v: '3', key: '12345', libraries: 'geometry,places' });
 });
--- imports/ui/pages/subscribe/subscribe.js
+++ imports/ui/pages/subscribe/subscribe.js
@@ -2,16 +2,12 @@ import './subscribe.html';
 
 var directionsDisplayArray = [];
 
-var ZOOM_LEVEL = 5; // ideal zoom level for streets
+var ZOOM_LEVEL = 5;
 var count = 0;
 var readyCount = 1;
 var renderedCount = 1;
 var routesCursor;
-
-Template.subscribe.rendered = function () {
-
-
-};
+var routesArray = [];
 
 Template.subscribe.helpers({
 
@@ -23,28 +19,52 @@ Template.subscribe.helpers({
 });
 /*
 
-Template.subscribe.events({
+ Template.subscribe.events({
 
-    'click #test'(event) {
-       // console.log(routesCursor.fetch()[20].mapRoute);
-        Meteor.call('dl');
-    }
+ 'click #test'(event) {
+ // console.log(routesCursor.fetch()[20].mapRoute);
+ Meteor.call('dl');
+ }
 
 
-});
-*/
+ });
+ */
 Template.routeMap.helpers({
     geolocationError: function () {
         var error = Geolocation.error();
         return error && error.message;
     },
     myMapOptions: function () {
+
         if (GoogleMaps.loaded()) {
 
             // Map options
             var mapOptions = {
                 zoom: ZOOM_LEVEL,
                 center: new google.maps.LatLng(6.523693, 80.096866),
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                disableDefaultUI: true,
+
+                // map controls
+                zoomControl: false,
+                mapTypeControl: true,
+                scaleControl: true,
+                streetViewControl: true,
+                rotateControl: true,
+                fullscreenControl: true,
+
+                // modify map type control
+                mapTypeControlOptions: {
+                    style: google.maps.MapTypeControlStyle.DEFAULT,
+                    position: google.maps.ControlPosition.LEFT_BOTTOM,
+                    mapTypeIds: [
+                        google.maps.MapTypeId.ROADMAP,
+                        google.maps.MapTypeId.HYBRID,
+                    ]
+                },
+                streetViewControlOptions: {
+                    position: google.maps.ControlPosition.RIGHT_TOP
+                }
             };
             return mapOptions;
         }
@@ -61,28 +81,32 @@ Template.routeMap.helpers({
 
 });
 
-Template.routeMap.onRendered(function () {
+Template.routeMap.onCreated(function () {
 
     var mapName = "map" + renderedCount;
     renderedCount++;
+    console.log("map "+ mapName+" created");
+
+
     GoogleMaps.ready(mapName, function (map) {
         console.log("map map" + readyCount + " is ready");
 
-            var display = new google.maps.DirectionsRenderer({
-                draggable: false,
-                map: map.instance,
-            });
+        var display = new google.maps.DirectionsRenderer({
+            draggable: false,
+            map: map.instance,
+        });
 
-            var temp = JSON.parse(routesCursor.fetch()[readyCount-1].mapRoute);
-            display.setDirections(temp);
+        var temp = JSON.parse(routesCursor.fetch()[readyCount - 1].mapRoute);
+        display.setDirections(temp);
 
-            directionsDisplayArray.push(display);
+        directionsDisplayArray.push(display);
 
         readyCount++;
     });
 
 
 });
+
 
 function setZm(mapName) {
     /*console.log(mapName.instance.getZoom());
